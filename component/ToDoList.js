f88b7bc905eb70741b057bba30e096bf749e95a0
parika054 <131924696+parika054@users.noreply.github.com>
2024-03-07 21:29:54 -0700

    component folder added

---
+++ component/ToDoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View,ScrollView, Text, Pressable, StyleSheet } from 'react-native';
+const ToDoList = ({tasks}) =>{
+    return(
+        <ScrollView>
+        {tasks.map((task, index) => (
+            <Pressable key={index}>
+                <View style={styles.task}>
+                    <Text style={styles.taskText}>{task}</Text>
+                </View>
+            </Pressable>
+        ))}
+    </ScrollView>
+    )
+}
+const styles = StyleSheet.create({
+    task: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#ccc',
+    },
+    completed: {
+        backgroundColor: '#e0e0e0',
+    },
+    taskText: {
+        fontSize: 16,
+    },
+});
+
+export default ToDoList;
